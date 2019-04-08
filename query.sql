b501fe3bcb5876a89fb4d7cf3ea6937739ae88a0
jps <jpsalo@zoho.com>
2019-04-08 17:34:44 +0300

    Return equal amount of stories per year

--- query.sql
+++ query.sql
@@ -1,26 +1,39 @@
 /*
 Using WHERE reduces the amount of data scanned / quota used
 */
+#standardSQL
 SELECT
   stories.score AS score,
   stories.descendants AS descendants,
-  EXTRACT(YEAR FROM DATE(stories.time_ts)) AS year,
+  stories.year AS year,
   stories.title AS title,
   CASE
-    WHEN STRPOS(title, "Ask HN: ") > 0 THEN
-      CASE
-        WHEN author = "whoishiring" OR author = "_whoishiring" THEN "whoishiring"
-        ELSE "ask"
-      END
-    WHEN STRPOS(title, "Show HN: ") > 0 THEN "show"
+    WHEN STRPOS(stories.title, "Ask HN: ") > 0 THEN CASE
+    WHEN stories.author = 'whoishiring'
+  OR stories.author = "_whoishiring" THEN "whoishiring"
+    ELSE "ask"
+  END WHEN STRPOS(stories.title, "Show HN: ") > 0 THEN "show"
     ELSE "story"
   END AS type
-FROM
-  `bigquery-public-data.hacker_news.stories` AS stories
+FROM (
+  SELECT
+    stories.score,
+    stories.descendants,
+    stories.title,
+    stories.author,
+    EXTRACT(YEAR
+    FROM
+      DATE(stories.time_ts)) AS year,
+    ROW_NUMBER() OVER(PARTITION BY EXTRACT(YEAR FROM DATE(stories.time_ts))
+    ORDER BY
+      stories.score DESC) AS rn
+  FROM
+    `bigquery-public-data.hacker_news.stories` AS stories ) AS stories
 WHERE
-  NOT (score IS NULL) AND
-  NOT (descendants IS NULL OR descendants = 0)
+  stories.rn <= 100
+  AND NOT (score IS NULL)
+  AND NOT (descendants IS NULL
+    OR descendants <= 0)
 ORDER BY
-  descendants DESC
-LIMIT
-  100
+  year DESC,
+  score DESC
