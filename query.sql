6b01553006484ae21caca3e8e0640c0f9b70a4b2
jps <jpsalo@zoho.com>
2019-04-09 15:32:09 +0300

    Get top results by score and descendants. See b501fe3bcb5876a89fb4d7cf3ea6937739ae88a0

--- query.sql
+++ query.sql
@@ -1,39 +1,69 @@
 /*
 Using WHERE reduces the amount of data scanned / quota used
 */
-#standardSQL
 SELECT
-  stories.score AS score,
-  stories.descendants AS descendants,
-  stories.year AS year,
-  stories.title AS title,
+  uni.score,
+  uni.descendants,
+  uni.year,
+  uni.title,
   CASE
-    WHEN STRPOS(stories.title, "Ask HN: ") > 0 THEN CASE
-    WHEN stories.author = 'whoishiring'
-  OR stories.author = "_whoishiring" THEN "whoishiring"
+    WHEN STRPOS(uni.title, "Ask HN: ") > 0 THEN CASE
+    WHEN uni.author = 'whoishiring'
+  OR uni.author = "_whoishiring" THEN "whoishiring"
     ELSE "ask"
-  END WHEN STRPOS(stories.title, "Show HN: ") > 0 THEN "show"
+  END WHEN STRPOS(uni.title, "Show HN: ") > 0 THEN "show"
     ELSE "story"
   END AS type
-FROM (
-  SELECT
-    stories.score,
-    stories.descendants,
-    stories.title,
-    stories.author,
-    EXTRACT(YEAR
-    FROM
-      DATE(stories.time_ts)) AS year,
-    ROW_NUMBER() OVER(PARTITION BY EXTRACT(YEAR FROM DATE(stories.time_ts))
-    ORDER BY
-      stories.score DESC) AS rn
-  FROM
-    `bigquery-public-data.hacker_news.stories` AS stories ) AS stories
-WHERE
-  stories.rn <= 100
-  AND NOT (score IS NULL)
-  AND NOT (descendants IS NULL
-    OR descendants <= 0)
+FROM ((
+    SELECT
+      stories.score AS score,
+      stories.descendants AS descendants,
+      stories.year AS year,
+      stories.author AS author,
+      stories.title AS title
+    FROM (
+      SELECT
+        stories.score,
+        stories.descendants,
+        stories.title,
+        stories.author,
+        EXTRACT(YEAR
+        FROM
+          DATE(stories.time_ts)) AS year,
+        ROW_NUMBER() OVER(PARTITION BY EXTRACT(YEAR FROM DATE(stories.time_ts))
+        ORDER BY
+          stories.score DESC) AS rn
+      FROM
+        `bigquery-public-data.hacker_news.stories` AS stories ) AS stories
+    WHERE
+      stories.rn <= 100
+      AND NOT (stories.score IS NULL
+        OR stories.score <= 0))
+  UNION DISTINCT (
+    SELECT
+      stories.score AS score,
+      stories.descendants AS descendants,
+      stories.year AS year,
+      stories.author AS author,
+      stories.title AS title
+    FROM (
+      SELECT
+        stories.score,
+        stories.descendants,
+        stories.title,
+        stories.author,
+        EXTRACT(YEAR
+        FROM
+          DATE(stories.time_ts)) AS year,
+        ROW_NUMBER() OVER(PARTITION BY EXTRACT(YEAR FROM DATE(stories.time_ts))
+        ORDER BY
+          stories.descendants DESC) AS rn
+      FROM
+        `bigquery-public-data.hacker_news.stories` AS stories ) AS stories
+    WHERE
+      stories.rn <= 100
+      AND NOT (stories.descendants IS NULL
+        OR stories.descendants <= 0))) AS uni
 ORDER BY
-  year DESC,
-  score DESC
+  uni.year DESC,
+  uni.descendants DESC
